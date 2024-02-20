20cbd8f9ed80a15683a41d48b9b239a8d6a341c9
WEB-TEACHER <mehmet-boz@hotmail.com.tr>
2024-02-20 17:44:49 +0300

    first commit

---
+++ lesson01/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson01.Models;
+
+namespace lesson01.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+    public IActionResult Gallery()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+
+}
